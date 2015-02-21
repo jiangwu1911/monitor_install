fce2fcf16a9e39fad13e842b8aac1b88f1faa8ca
wujiang <jiangwu1911@hotmail.com>
2015-02-22 00:48:08 +0800

    add monit tomcat

--- build/monitor_puppet/modules/monitor/manifests/init.pp
+++ build/monitor_puppet/modules/monitor/manifests/init.pp
@@ -1,4 +1,9 @@
-class logstash {
+class monitor {
+    service { 'mysql':
+        ensure => stopped,
+	    enable => false,
+    } 
+
     service { 'monit':
         ensure => running,
         enable => true,
@@ -6,6 +11,11 @@ class logstash {
 
     file { '/etc/monit.d/mysql':
         source => 'puppet:///modules/monitor/monit.d/mysql',
+        notify => Service['monit'],
+    }
+
+    file { '/etc/monitrc':
+        source => 'puppet:///modules/monitor/monitrc',
         notify => Service['monit'],
     }
 }
